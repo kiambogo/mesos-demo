e729178f782c419771a8823b4afbbbb4331eac7a
Christopher Poenaru <kiambogo@gmail.com>
2016-07-10 21:22:13 -0400

    compose w/ mesos, marathon, bamboo

---
+++ webapp/app/controllers/ApplicationController.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api.mvc._
+import javax.inject._
+import java.rmi.dgc.VMID
+
+class ApplicationController @Inject() extends Controller {
+  def index = Action {
+    Ok(s"Hello Gilt from ${new VMID().toString.take(10)}")
+  }
+}
---
+++ webapp/build.sbt
@@ -0,0 +1,11 @@
+name := "demo"
+
+version := "1.1"
+
+scalaVersion := "2.11.8"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+libraryDependencies ++= Seq(
+  ws
+)
